3aca26c8816d7cdfbcfd9333e80038bf18f4d35b
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 07:04:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,6 +4,28 @@ end
 
 def sort_array_desc(intarray)
   intarray.sort do |a,b|
-    b <=> a 
+    b <=
   end
+end
+  
+def sort_array_char_count(stringarray)
+   stringarray.sort do |a,b|
+     a.length <=> b.length
+   end
+end
     
+def swap_elements(array)
+  a = 0
+  collection = []
+  while a < array.size do
+    if a == 2 then
+      collection.push(array[3])
+    elsif a == 3 then
+      collection.push(array[2])
+    else
+      collection.push(array[a])
+    end
+  end
+  collection
+end
+
